c51b034d96df29d9afefa31b53335f3593db93f3
LegradiK <legradi.kaho+github@gmail.com>
2026-05-08 15:12:26 +0100

    changes

--- dark_side.py
+++ dark_side.py
@@ -2,10 +2,10 @@ import pygame
 from config import SCREEN_WIDTH, SCREEN_HEIGHT
 
 class DarkSide:
-    ICON_SIZE_WIDTH = 54
-    ICON_SIZE_HEIGHT = 54
+    ICON_SIZE = 54
     ROWS = 5
     COLUMNS = 11
+    SPEED = 5
 
     def __init__(self):
         self.image = None
@@ -14,22 +14,24 @@ class DarkSide:
     def load_image(self):
         # load and resize enemy (dark side)
         image = pygame.image.load("static/tie-fighter.png").convert_alpha()
-        self.image = pygame.transform.scale(image, (DarkSide.ICON_SIZE_WIDTH, DarkSide.ICON_SIZE_HEIGHT))
+        self.image = pygame.transform.scale(image, (DarkSide.ICON_SIZE, DarkSide.ICON_SIZE_HEIGHT))
         self.build_fleet()
     
     def build_fleet(self):
-        total_width = DarkSide.COLUMNS * (DarkSide.ICON_SIZE_WIDTH)
+        total_width = DarkSide.COLUMNS * (DarkSide.ICON_SIZE)
         start_x = (SCREEN_WIDTH - total_width) // 2
 
         for row in range(DarkSide.ROWS):
             for col in range(DarkSide.COLUMNS):
-                x = start_x + col * (DarkSide.ICON_SIZE_WIDTH)
-                y = 20 + row * (DarkSide.ICON_SIZE_HEIGHT)
+                x = start_x + col * (DarkSide.ICON_SIZE)
+                y = 20 + row * (DarkSide.ICON_SIZE)
                 self.fleet.append([x, y])
 
     def draw(self, screen):
         for x, y in self.fleet:
             screen.blit(self.image, (x, y))
     
-
-
+    def move(self):
+        # not to exceed side
+        self.x = max(0, min(self.x, SCREEN_WIDTH - DarkSide.ICON_SIZE))
+        self.y = max(0, min(self.y, SCREEN_HEIGHT - DarkSide.ICON_SIZE))
